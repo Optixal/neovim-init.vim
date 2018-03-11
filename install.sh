11e29d3707b61a11ca1d944ec3667df05cbdc7be
Shawn Pang <optixal@live.com.sg>
2018-03-11 18:28:45 +0800

    Added UpdateRemotePlugins to install script

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ echo "alias vim='nvim'" >> ~/.bashrc &&\
 # Enter Neovim and install plugins using a temporary init.vim, which avoids warnings about missing colorschemes, functions, etc
 echo -e '[*] Running :PlugInstall within nvim ...' &&\
 sed '/call plug#end/q' init.vim > ~/.config/nvim/init.vim &&\
-nvim -c ':PlugInstall' -c ':qall' &&\
+nvim -c ':PlugInstall' -c ':UpdateRemotePlugins' -c ':qall' &&\
 rm ~/.config/nvim/init.vim &&\
 
 # Copy init.vim in current working directory to nvim's config location ...
