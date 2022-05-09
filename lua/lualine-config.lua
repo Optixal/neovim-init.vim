281f9ac5c5a88f6eccc6ee5253ce313d5178d26e
Optixal <optixal@live.com.sg>
2022-05-09 19:01:24 +0800

    fix: hide lualine in nvim tree

--- lua/lualine-config.lua
+++ lua/lualine-config.lua
@@ -6,7 +6,7 @@ require('lualine').setup {
         component_separators = { left = '╲', right = '╱' },
         --section_separators = { left = '', right = ''},
         section_separators = { left = '', right = '' },
-        disabled_filetypes = {},
+        disabled_filetypes = { 'NvimTree' },
         always_divide_middle = true,
         globalstatus = false,
     },
