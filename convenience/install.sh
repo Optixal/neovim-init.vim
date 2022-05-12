de83d4861854888bc189c00178dd0e2115dc45cf
Optixal <optixal@live.com.sg>
2022-05-12 17:45:07 +0800

    fix: wget command to download to right location during installation

--- convenience/install.sh
+++ convenience/install.sh
@@ -57,7 +57,7 @@ fi
 
 # Install neovim
 echo "[*] Installing neovim $NEOVIM_VERSION ..."
-wget "https://github.com/neovim/neovim/releases/download/v$NEOVIM_VERSION/nvim-linux64.tar.gz" -P /tmp
+wget "https://github.com/neovim/neovim/releases/download/v$NEOVIM_VERSION/nvim-linux64.tar.gz" -O /tmp/nvim-linux64.tar.gz
 mkdir -p ~/.local/bin
 tar xf /tmp/nvim-linux64.tar.gz -C ~/.local
 ln -sf $(readlink -f ~/.local/nvim-linux64/bin/nvim) ~/.local/bin/nvim
